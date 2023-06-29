81f27e3a96fe4f4ccd78c400c42defb4c133a138
Hasan Çelik <hasancelikdeveloper@gmail.com>
2023-06-29 22:31:23 +0300

    project created

---
+++ src/main/java/com/hasanceliktr/connectshare/ConnectShareApplication.java
@@ -0,0 +1,13 @@
+package com.hasanceliktr.connectshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasanceliktr/connectshare/ConnectShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasanceliktr.connectshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
